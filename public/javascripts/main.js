f9bc843b8616ff12395c0d516d4fb82dfbaeeaab
Jesse Warren <jesse@jesse.warren>
2014-02-07 19:20:27 -0800

    added socket.on newMessage and matched to main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,51 +12,49 @@ $(function () {
 	socket.on('connect', function(){
 		sendMessage('socket.io', 'Socket Power!');
 		socket.emit('addUser', username);
-
-		// Events
-		$(window).keypress(function (e) {
-			var enter = e.keyCode === 13; // Enter
-			if (enter) {
-				var text = $chatInput.val();
-				$chatInput.val('');
-				sendMessage(text);
-			}
 		});
+	});
 
-		$(window).click(function () {
-			$chatInput.focus();
-		});
-
-
-		// Sends message to other user
-		function sendMessage (message) {
-			// Socketio stuff
-			postMessage(username, message);
-			socket.emit('sendMessage', username, message);
+	// Events
+	$(window).keypress(function (e) {
+		var enter = e.keyCode === 13; // Enter
+		if (enter) {
+			var text = $chatInput.val();
+			$chatInput.val('');
+			sendMessage(text);
 		}
+	});
 
-		function postMessage (username, message) {
-			var $usernameArea = $('<div/>').addClass('username').html(username);
-			var $messageArea = $('<div/>').addClass('messageArea').html(message);
-			var $message = $('<li/>').append([$usernameArea, $messageArea]);
-			$chatArea.find('.messages').append($message);
-
-			// Scroll to bot
-			$chatArea[0].scrollTop = $chatArea[0].scrollHeight;
-		}
-
-		// post messages sent by otherUser
-		socket.on('newMessage', function(data) {
-			postMessage(otherUser, data);
-		});
-
-		// let user know who he just connected with
-		socket.on('matched', function(name) {
-			otherUser = name;
-			var matchedMessage = "You have been matched with " + otherUser + "!!!";
+	$(window).click(function () {
+		$chatInput.focus();
+	});
 
 
-		});
+	// Sends message to other user
+	function sendMessage (message) {
+		// Socketio stuff
+		postMessage(username, message);
+		socket.emit('sendMessage', username, message);
+	}
 
+	function postMessage (username, message) {
+		var $usernameArea = $('<div/>').addClass('username').html(username);
+		var $messageArea = $('<div/>').addClass('messageArea').html(message);
+		var $message = $('<li/>').append([$usernameArea, $messageArea]);
+		$chatArea.find('.messages').append($message);
+
+		// Scroll to bot
+		$chatArea[0].scrollTop = $chatArea[0].scrollHeight;
+	}
+
+	// post messages sent by otherUser
+	socket.on('newMessage', function(data) {
+		postMessage(otherUser, data);
+	});
+
+	// let user know who he just connected with
+	socket.on('matched', function(name) {
+		otherUser = name;
+		var matchedMessage = "You have been matched with " + otherUser + "!!!";
 	});
 });
